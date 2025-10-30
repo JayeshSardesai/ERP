a2df1497a80733592b9540a642129d513f8f56f6
amanshinde <shindeaman31@gmail.com>
2025-10-30 12:46:25 +0530

    leave page admin

--- frontend/src/roles/admin/AdminApp.tsx
+++ frontend/src/roles/admin/AdminApp.tsx
@@ -36,6 +36,8 @@ import FeesPage from './pages/FeesPage'
 
 import ReportsPage from './pages/ReportsPage'
 
+import LeaveManagement from './pages/LeaveManagement'
+
 import ErrorBoundary from '../../components/ErrorBoundary'
 
 import { PermissionGuard } from '../../components/PermissionGuard'
@@ -166,6 +168,14 @@ export function AdminApp() {
           <Route path="reports" element={
             <PermissionGuard permission="viewReports" permissionName="Reports">
               <ReportsPage />
+            </PermissionGuard>
+
+          } />
+
+          {/* Leave Management - Requires manageUsers permission */}
+          <Route path="leave-management" element={
+            <PermissionGuard permission="manageUsers" permissionName="Leave Management">
+              <LeaveManagement />
             </PermissionGuard>
 
           } />
